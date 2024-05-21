9e64c3be7da8ca44d976e99aefa8c990243e6dfb
s.gudivada@techmitts.com <s.gudivada@techmitts.com>
2024-05-21 18:05:30 +0530

    new application

---
+++ my-node-app/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://mongo:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
